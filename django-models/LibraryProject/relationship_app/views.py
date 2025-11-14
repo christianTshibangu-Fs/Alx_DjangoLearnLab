e49475f182105b2ad236828cb7f0c7bdf24f24db
christian tshibangu <christshims2015@gmail.com>
2025-11-14 19:44:14 +0100

    Update UserProfile feature

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book, Librarian
@@ -35,12 +36,15 @@ def home_page(request):
 
 # (Vous devez créer un template home.html si vous utilisez cette fonction)
 
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Librarian')    
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Member')   
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
