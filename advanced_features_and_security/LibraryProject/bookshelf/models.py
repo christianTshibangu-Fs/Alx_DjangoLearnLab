20d42d97d4ffc32f1f9db948c7b1d871be0a5c49
christian tshibangu <christshims2015@gmail.com>
2025-11-16 16:20:14 +0100

    Update managing permission and groups

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -30,7 +30,7 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(username, email, password, **extra_fields)
 
-class Book (models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
