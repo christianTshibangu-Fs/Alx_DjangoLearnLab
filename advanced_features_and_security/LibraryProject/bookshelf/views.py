a32dd0bbee4f5e61e1bd64b0d51e76e37aff9698
christian tshibangu <christshims2015@gmail.com>
2025-11-16 18:17:11 +0100

    Update security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .form import ExempleForm
+from .forms import ExempleForm
 
 # Create your views here.
 
