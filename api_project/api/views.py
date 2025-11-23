cc202115a88cb88d05b4a1504dadcc5a9b2c8dd7
christian tshibangu <christshims2015@gmail.com>
2025-11-23 12:24:37 +0100

    Add CRUD api to model Book

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
@@ -7,5 +8,9 @@ from .models import Book
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
