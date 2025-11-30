8e51f1117abedf23e3be06f811983a09372063c9
christian tshibangu <christshims2015@gmail.com>
2025-11-30 14:49:16 +0100

    Advanced-api-project

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-    published_date = models.DateField()
+    publication_year = models.DateField()
 
     def __str__(self):
         return self.title
