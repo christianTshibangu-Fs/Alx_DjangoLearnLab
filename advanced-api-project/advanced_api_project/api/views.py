9d348bb5bc6f58774290a9b5be837c4296d44f6c
christian tshibangu <christshims2015@gmail.com>
2025-11-30 14:31:04 +0100

    Advanced-api-project

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
 # Create your views here.
+
+class BookListView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
