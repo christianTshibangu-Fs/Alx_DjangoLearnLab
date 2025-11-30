142eb904360e149be1c6657ccc6862d9a35fe22d
christian tshibangu <christshims2015@gmail.com>
2025-11-30 15:03:49 +0100

    Advanced-api-project

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Author, Book
+from datetime import datetime
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +13,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'published_date']
+
+    def validate(self, attrs):
+        if 'published_date' in attrs:
+            if attrs['published_date'] > datetime.date.today():
+                raise serializers.ValidationError("Publication date cannot be in the future.")
+        return super().validate(attrs)
