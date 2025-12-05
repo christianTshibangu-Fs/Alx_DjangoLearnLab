38c91b09388f8efc7b6fe30a3bf57ecc59ecad4f
christian tshibangu <christshims2015@gmail.com>
2025-12-05 12:54:09 +0100

    add blog User's authentification system

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name='home'), # Home page route
+    # You can add more blog-related routes here
+    path('register/', views.register, name='register'), # Registration route
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'), # Login route
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'), # Logout route
+
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+
+def home_page(request):
+    return render(request, 'blog/home.html')
+
+# Vue d'inscription personnalisée
+def register(request):
+    """Gère l'inscription de nouveaux utilisateurs."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Connecte l'utilisateur immédiatement après l'inscription
+            login(request, user)
+            # Redirige vers la page d'accueil ou une autre page après l'inscription
+            return redirect('/') 
+    else:
+        form = UserCreationForm()
+    
+    # Rend le template 'register.html' en passant le formulaire
+    return render(request, 'blog/register.html', {'form': form})
